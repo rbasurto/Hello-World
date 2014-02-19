bd5d7ae945d3e160389af5256492c5f885f46b2f
Rene Alejandro Basurto Quijada <rbasurto@nutzer.com.mx>
2014-02-18 18:02:23 -0600

    Hello World iOS 7 en español

---
+++ Hello World/HWViewController.m
@@ -0,0 +1,29 @@
+//
+//  HWViewController.m
+//  Hello World
+//
+//  Created by Rene Alejandro Basurto Quijada on 10/02/14.
+//  Copyright (c) 2014 Rene A Basurto Q. All rights reserved.
+//
+
+#import "HWViewController.h"
+
+@interface HWViewController ()
+
+@end
+
+@implementation HWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
